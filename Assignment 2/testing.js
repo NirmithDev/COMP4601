f17ba1da18d9459045689967e43b1ab546080655
Johnathan Scaife <johnwscaife@gmail.com>
2023-12-04 18:40:44 -0500

    Done

--- Assignment 2/testing.js
+++ Assignment 2/testing.js
@@ -21,7 +21,7 @@ function convertArrayToCSV(array) {
 }
 
 function main() {
-    const topKParameters = [5,10,20,40,60,80,100];
+    const topKParameters = [5,10,20,40,60,80,100,200,250,300,350,400,425,450,500];
     const thresholdParameters = [0, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 0.9, 1];
 
     let topKItemBased = [];
@@ -31,7 +31,7 @@ function main() {
     let thresholdAboveUserBased = [];
     let thresholdBelowUserBased = [];
 
-    const filePath = path.join(__dirname, 'parsed-data-trimmed.txt');
+    const filePath = path.join(__dirname, 'assignment2-data.txt');
     const matrix = itemBased.createMatrix(filePath);
 
     for (let i = 0; i < topKParameters.length; i++) {
@@ -39,12 +39,14 @@ function main() {
         let start = Date.now();
         let temp = itemBased.leaveOneOut(matrix, 'topK', topKParameters[i]);
         let x = Date.now() - start;
+        console.log(x);
         topKItemBased.push([topKParameters[i], temp.MAE, x, temp.totalErrors, temp.predictionsMade]);
 
         // TopK user based
         start = Date.now();
         temp = userBased.leaveOneOut(matrix[2], 'topK', topKParameters[i]);
         x = Date.now() - start;
+        console.log(x);
         topKUserBased.push([topKParameters[i], temp, x]);
     }
     for (let i = 0; i < thresholdParameters.length; i++) {
@@ -52,24 +54,28 @@ function main() {
         start = Date.now();
         let temp = itemBased.leaveOneOut(matrix, 'threshold-above', thresholdParameters[i]);
         let x = Date.now() - start;
+        console.log(x);
         thresholdAboveItemBased.push([thresholdParameters[i], temp.MAE, x, temp.totalErrors, temp.predictionsMade]);
 
         // Threshold below item based
         start = Date.now();
         temp = itemBased.leaveOneOut(matrix, 'threshold-below', thresholdParameters[i]);
         x = Date.now() - start;
+        console.log(x);
         thresholdBelowItemBased.push([thresholdParameters[i], temp.MAE, x, temp.totalErrors, temp.predictionsMade]);
 
         // Threshold above user base
         start = Date.now();
         temp = userBased.leaveOneOut(matrix[2], 'threshold-above', thresholdParameters[i]);
         x = Date.now() - start;
+        console.log(x);
         thresholdAboveUserBased.push([thresholdParameters[i], temp, x]);
 
         // Threshold below user base
         start = Date.now();
         temp = userBased.leaveOneOut(matrix[2], 'threshold-below', thresholdParameters[i]);
         x = Date.now() - start;
+        console.log(x);
         thresholdBelowUserBased.push([thresholdParameters[i], temp, x]);
     }
 
@@ -91,12 +97,12 @@ function main() {
     console.log('\n');
     console.log('============================================================');
 
-    fs.writeFileSync('topKItemBased', convertArrayToCSV(topKItemBased), 'utf-8');
-    fs.writeFileSync('thresholdAboveItemBased', convertArrayToCSV(thresholdAboveItemBased), 'utf-8');
-    fs.writeFileSync('thresholdBelowItemBased', convertArrayToCSV(thresholdBelowItemBased), 'utf-8');
-    fs.writeFileSync('topKUserBased', convertArrayToCSV(topKUserBased), 'utf-8');
-    fs.writeFileSync('thresholdAboveUserBased', convertArrayToCSV(thresholdAboveUserBased), 'utf-8');
-    fs.writeFileSync('thresholdBelowUserBased', convertArrayToCSV(thresholdBelowUserBased), 'utf-8');
+    fs.writeFileSync('topKItemBased.csv', convertArrayToCSV(topKItemBased), 'utf-8');
+    fs.writeFileSync('thresholdAboveItemBased.csv', convertArrayToCSV(thresholdAboveItemBased), 'utf-8');
+    fs.writeFileSync('thresholdBelowItemBased.csv', convertArrayToCSV(thresholdBelowItemBased), 'utf-8');
+    fs.writeFileSync('topKUserBased.csv', convertArrayToCSV(topKUserBased), 'utf-8');
+    fs.writeFileSync('thresholdAboveUserBased.csv', convertArrayToCSV(thresholdAboveUserBased), 'utf-8');
+    fs.writeFileSync('thresholdBelowUserBased.csv', convertArrayToCSV(thresholdBelowUserBased), 'utf-8');
 }
 
 main();
